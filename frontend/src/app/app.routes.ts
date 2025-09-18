547f54531354d5114d675b27e39d052d9eb7062b
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-09-18 15:46:47 +0300

    jwt problem solved, frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { SpotifyCbComponent } from './spotify-cb/spotify-cb.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     component: PlaylistComponent,
     path: 'playlists',
+  },
+  {
+    component: SpotifyCbComponent,
+    path: 'spotifycb',
   },
 ];
--- frontend/src/app/playlist/playlist.component.ts
+++ frontend/src/app/playlist/playlist.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-playlist',
   imports: [],
   templateUrl: './playlist.component.html',
-  styleUrl: './playlist.component.css'
+  styleUrl: './playlist.component.css',
 })
-export class PlaylistComponent {
-
+export class PlaylistComponent implements OnInit {
+  ngOnInit(): void {
+    axios
+      .get('http://127.0.0.1:8888/playlists', { withCredentials: true })
+      .then((response) => console.log(response));
+  }
 }
