7418e23b8d6e28887f81b5c815583e2e0e526063
Lasis7 <AE8656@student.jamk.fi>
2025-10-06 15:15:36 +0300

    Continued making song cleaning page

--- frontend/src/app/playlistclean/playlistclean.component.ts
+++ frontend/src/app/playlistclean/playlistclean.component.ts
@@ -3,15 +3,17 @@ import { inject } from '@angular/core';
 import { profileStore } from '../utilities/stores/profile.store';
 import { RouterLink } from '@angular/router';
 import { playlistStore } from '../utilities/stores/playlist.store';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-playlistclean',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './playlistclean.component.html',
   styleUrl: './playlistclean.component.css',
 })
 export class PlaylistcleanComponent implements OnInit {
   title: string = 'WaveSpacer';
+  checked: boolean = false;
 
   profileStore = inject(profileStore);
   playlistStore = inject(playlistStore);
