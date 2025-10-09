9443f1e6ac264c0aedc466062b696e089366dae1
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-10-09 21:33:19 +0300

    aws test push 5

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { startCronJobs } = require('../src/jobs/recentlyPlayedJob');
 
-// aws test push4
+// aws test push5
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
