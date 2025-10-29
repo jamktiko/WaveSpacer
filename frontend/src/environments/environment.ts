c32928539000002fa9eee58e7fd7c032c4d2ee24
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-10-29 14:27:32 +0200

    ec2 link changed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-16-171-232-32.eu-north-1.compute.amazonaws.com/',
+  apiUrl: 'http://ec2-16-16-67-224.eu-north-1.compute.amazonaws.com/',
 };
