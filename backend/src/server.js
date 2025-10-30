dffaa98e24aa0bad3c7bfb8bf4be384ad00e658b
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-10-30 10:08:53 +0200

    8888 port

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ async function startServer() {
   await pool.initPool();
 
   //Käynnistetään palvelin
-  const PORT = process.env.PORT || 80;
+  const PORT = process.env.PORT || 8888;
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running at port ${PORT}`);
   });
