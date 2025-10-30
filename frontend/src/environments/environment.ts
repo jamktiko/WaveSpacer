d70b54343ef01630e09722d0968ba1bd7d70afd2
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-10-30 16:10:07 +0200

    back to 443

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-16-16-67-224.eu-north-1.compute.amazonaws.com:8888/',
+  apiUrl: 'https://ec2-16-16-67-224.eu-north-1.compute.amazonaws.com/',
 };
