99d7e47bc4fbd05b34245dc99852295f1d043bd5
Matias Jylhä <matias.ta.jylha@gmail.com>
2025-11-03 10:31:19 +0200

    sending cookie changed

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -7,8 +7,8 @@ exports.loginWithSpotify = async (userID, tokens, res) => {
   // lähetä cookie
   res.cookie('jwt', jwtToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     path: '/',
     maxAge: 4 * 60 * 60 * 1000,
   });
