48f989820abd840bb5c9ceb0d4645740661baa2d
Matias <matias.ta.jylha@gmail.com>
2025-11-06 09:57:36 +0200

    cron in aws scheduled to run every hour

--- backend/src/jobs/recentlyPlayedJob.js
+++ backend/src/jobs/recentlyPlayedJob.js
@@ -2,11 +2,19 @@ const cron = require('node-cron');
 const spotifyController = require('../controllers/spotifyController');
 
 function startCronJobs() {
-  cron.schedule('*/1 * * * *', async () => {
-    console.log('Ajetaan cron: fetchRecentsForAllUsers()');
-    const result = await spotifyController.fetchRecentsForAllUsers();
-    console.log('Cron valmis:', result);
-  });
+  if (process.env.NODE_ENV === 'development') {
+    cron.schedule('*/1 * * * *', async () => {
+      console.log('Ajetaan cron: fetchRecentsForAllUsers()');
+      const result = await spotifyController.fetchRecentsForAllUsers();
+      console.log('Cron valmis:', result);
+    });
+  } else {
+    cron.schedule('0 * * * *', async () => {
+      console.log('Ajetaan cron: fetchRecentsForAllUsers()');
+      const result = await spotifyController.fetchRecentsForAllUsers();
+      console.log('Cron valmis:', result);
+    });
+  }
 }
 
 module.exports = { startCronJobs };
